42f0ba5d1c0ce3fb4cf60eb2c7a821f977d597b2
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-02-23 16:22:55 +0400

    added timestamp mixins for all models

---
+++ crm_service/mixins/timestamp.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.db import models
+
+
+class TimestampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='created_%(class)s')
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='updated_%(class)s')
+
+    class Meta:
+        abstract = True
+        indexes = [
+            models.Index(fields=['created_at']),
+            models.Index(fields=['updated_at']),
+        ]
+
+    def __str__(self):
+        return f"{self.__class__.__name__} created at {self.created_at}"
