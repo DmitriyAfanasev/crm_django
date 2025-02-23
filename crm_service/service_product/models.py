6a16b05ff303f0a1d265ae6048618b11b085b430
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-02-23 18:44:14 +0400

    fixed model product

--- crm_service/service_product/models.py
+++ crm_service/service_product/models.py
@@ -9,27 +9,55 @@ from mixins import TimestampMixin
 class Product(TimestampMixin, models.Model):
     """Модель услуги для клиентов."""
 
-    name = models.CharField(max_length=100, blank=False, null=False, db_index=True)
-    description = models.TextField(blank=False, null=False)
+    name = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        db_index=True,
+        verbose_name=_("Service name"),
+    )
+    description = models.TextField(
+        blank=False,
+        null=False,
+        verbose_name=_("Service description"),
+    )
     cost = models.FloatField(
-        validators=[MinValueValidator(0.0)], blank=False, null=False
+        validators=[MinValueValidator(0.0)],
+        blank=False,
+        null=False,
+        verbose_name=_("Service cost"),
     )
     discount = models.PositiveSmallIntegerField(
-        default=0, validators=[MinValueValidator(0), MaxValueValidator(50)]
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(50)],
+        blank=False,
+        null=False,
+        verbose_name=_("Discount percentage"),
     )
     status = models.CharField(
         max_length=20,
+        default="inactive",
+        blank=False,
+        null=False,
+        verbose_name=_("Service status"),
         choices=[
             ("active", _("Active")),
             ("inactive", _("Inactive")),
+            ("in_development", _("In-Development")),
         ],
-        default="inactive",
     )
+    archived = models.BooleanField(default=False, verbose_name=_("Archived"))
 
-    def __str__(self):
-        return self.name, self.status
+    class Meta:
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
+        ordering = ("name", "created_at")
+        db_table = "service_products"
+
+    def __str__(self) -> str:
+        return self.name
 
     @property
-    def final_cost(self):
+    def final_cost(self) -> float:
         """Рассчитывает итоговую стоимость с учётом скидки."""
         return self.cost * (1 - self.discount / 100)
