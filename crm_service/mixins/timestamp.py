12e2bb996d3df3d2517a98c0680b477e88b973b2
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-02-23 19:47:27 +0400

    added full tipe hint and docstrings for module

--- crm_service/mixins/timestamp.py
+++ crm_service/mixins/timestamp.py
@@ -1,19 +1,31 @@
+from django.db.models import ForeignKey, DateTimeField, Index
 from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from django.db import models
 
 
 class TimestampMixin(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(
+    """
+    Миксин для добавления полей отслеживания создания и обновления моделей,
+    а также отслеживания, кто создаёт и обновляет модель.
+
+    Поля:
+        created_at (DateTimeField): Дата и время создания объекта
+        updated_at (DateTimeField): Дата и время последнего обновления объекта
+        created_by (ForeignKey): Пользователь, создавший объект
+        updated_by (ForeignKey): Пользователь, обновивший объект
+    """
+
+    created_at: DateTimeField = models.DateTimeField(auto_now_add=True)
+    updated_at: DateTimeField = models.DateTimeField(auto_now=True)
+    created_by: ForeignKey | None = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         null=True,
         related_name="created_%(class)s",
         verbose_name=_("created by"),
     )
-    updated_by = models.ForeignKey(
+    updated_by: ForeignKey | None = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         null=True,
@@ -22,11 +34,16 @@ class TimestampMixin(models.Model):
     )
 
     class Meta:
-        abstract = True
-        indexes = [
+        """
+        Мета информация.
+        Ключевым является комментарий, чтобы указать, что это мета-опции для абстрактного класса.
+        """
+
+        abstract: bool = True
+        indexes: list[Index] = [
             models.Index(fields=["created_at"]),
             models.Index(fields=["updated_at"]),
         ]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.__class__.__name__} created at {self.created_at}"
