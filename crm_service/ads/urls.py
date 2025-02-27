d7cb582e153c37f03136a6589909d07de29068f2
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-02-27 16:27:08 +0400

    added detail view for company

--- crm_service/ads/urls.py
+++ crm_service/ads/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import AdsCompanyListView, AdsCompanyCreateView
+from .views import AdsCompanyListView, AdsCompanyCreateView, AdsCompanyDetailView
 
 app_name: str = "ads"
 
 urlpatterns: list[path] = [
     path("", AdsCompanyListView.as_view(), name="ads_list"),
     path("new/", AdsCompanyCreateView.as_view(), name="ads_create"),
+    path("<int:pk>/", AdsCompanyDetailView.as_view(), name="ads_detail"),
 ]
--- crm_service/ads/views.py
+++ crm_service/ads/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     CreateView,
@@ -11,10 +13,24 @@ from .models import AdsCompany
 from .forms import AdsCompanyCreateForm
 
 
+# TODO добавить пермишены
 class AdsCompanyListView(ListView):
+    """Представление для списка всех рекламных компаний в системе."""
+
     model: AdsCompany = AdsCompany
 
 
 class AdsCompanyCreateView(CreateView):
+    """Представление для маркетологов и всех у кого есть право на создание рекламных компаний."""
+
     model: AdsCompany = AdsCompany
-    form_class = AdsCompanyCreateForm
+    form_class: AdsCompanyCreateForm = AdsCompanyCreateForm
+
+    def get_success_url(self) -> HttpResponseRedirect:
+        """При успешном создании услуги, перенаправляет на страницу с деталями этой услуги."""
+        return reverse_lazy("ads:ads_detail", kwargs={"pk": self.object.pk})
+
+
+class AdsCompanyDetailView(DetailView):
+    model: AdsCompany = AdsCompany
+    context_object_name: str = "company"
