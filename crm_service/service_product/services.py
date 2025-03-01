bccaa368ffc51d1e630509180b7a8acacff8c0f8
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-03-01 19:02:43 +0400

    fixed structure validation for create Product aka service

--- crm_service/service_product/services.py
+++ crm_service/service_product/services.py
@@ -1,20 +1,16 @@
-from django.conf import settings
 from django.contrib.auth.models import User
-from django.core.cache import cache
 from django.utils.translation import gettext_lazy as _
 
-from pydantic import BaseModel, Field
-from pathlib import Path
+from pydantic import Field
 
+from utils.mixins.services_mixins import BadWordsMixin
 from .dto_product import ProductCreateDTO
 from .models import Product
-
-
-class BaseProduct(BaseModel): ...
+from core.check_user_service import UserRoleService
 
 
 # TODO можно будет добавить рейтинг для заказчиков, и если у них хороший рейтинг, делать скидку или брать их заказы из очереди первыми.
-class ProductService(BaseProduct):
+class ProductService(BadWordsMixin, UserRoleService):
     name: str = Field(min_length=3, max_length=100)
     description: str
     cost: float
@@ -24,46 +20,31 @@ class ProductService(BaseProduct):
     created_by: int
 
     @staticmethod
-    def _load_bad_words() -> set[str]:
-        """Загружает список запрещенных слов из файла."""
-
-        file_with_words: Path = settings.BAD_WORDS_FILE
-        with open(file=file_with_words, mode="r", encoding="utf-8") as file:
-            return {word.strip().lower() for word in file.readlines()}
-
-    @staticmethod
-    def _check_for_bad_words(text: str, bad_words: set[str]) -> bool:
-        """Проверяет, содержит ли текст запрещенные слова."""
-        text_lower: str = text.lower()
-        new_text = set(text_lower.split())  # Преобразуем список слов в множество
-        return not new_text.isdisjoint(bad_words)  # Проверяем на пересечение
-
-    @staticmethod
     def checking_before_creation(data_from_request: ProductCreateDTO) -> None:
-        """Проверяет данные перед созданием продукта."""
+        ProductService._check_existing_name_by_field_in_db(
+            Product, data_from_request.name, _("This service is already registered.")
+        )
 
-        if Product.objects.filter(name=data_from_request.name).exists():
-            raise ValueError(_("This service is already registered."))
+        bad_words: set[str] = ProductService._get_bad_words()
 
+        ProductService._check_field_for_bad_words(
+            "name",
+            data_from_request.name,
+            bad_words,
+        )
+        ProductService._check_field_for_bad_words(
+            "description",
+            data_from_request.description,
+            bad_words,
+        )
         if data_from_request.discount >= data_from_request.cost:
             raise ValueError(_("Discount cannot be greater than or equal to cost."))
 
         if data_from_request.archived and data_from_request.status == "active":
             raise ValueError(_("Archived products cannot be active."))
 
-        creator = User.objects.filter(id=data_from_request.created_by, is_active=True)
-        if not creator:
-            raise ValueError(_("The creator must be an active user."))
+        ProductService._check_active_user(user_id=data_from_request.created_by)
 
-        bad_words: set[str] | None = cache.get("bad_words")
-        if bad_words is None:
-            bad_words: set[str] = ProductService._load_bad_words()
-            cache.set("bad_words", bad_words, timeout=7200)
-
-        if ProductService._check_for_bad_words(data_from_request.name, bad_words):
-            raise ValueError(_("Name contains forbidden words."))
-
-        if ProductService._check_for_bad_words(
-            data_from_request.description, bad_words
-        ):
-            raise ValueError(_("Description contains forbidden words."))
+        user = User.objects.get(id=data_from_request.created_by)
+        service_name = ProductService._get_service_name()
+        ProductService._check_user_role(user=user, service_name=service_name)
