5729f58cbb74f254b80dc87f2c6a7e9bc340e906
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-03-03 01:07:37 +0400

    added Customer in admin panel

--- crm_service/customers/admin.py
+++ crm_service/customers/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "lead",
+        "created_at",
+        "updated_at",
+        "created_by",
+        "updated_by",
+        "full_name",
+    )
+    list_display_links = (
+        "pk",
+        "lead",
+    )
+    search_fields = ("pk", "lead__first_name", "lead__last_name")
+
+    @admin.display(description="Full name")
+    def full_name(self, obj: Customer) -> str:
+        return obj.lead.abbreviated_name
