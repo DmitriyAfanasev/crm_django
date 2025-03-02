044b5889938c5ffa6c328a9d7aeda8971585470c
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-03-02 18:35:48 +0400

    added model active-client aka customer

--- crm_service/customers/models.py
+++ crm_service/customers/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models import BooleanField
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from utils.mixins import TimestampMixin
+from leads.models import Lead
+
+
+class Customer(TimestampMixin, models.Model):
+    """Активный клиент пользующийся услугами компаний."""
+
+    lead: Lead = models.OneToOneField(
+        to=Lead,
+        on_delete=models.PROTECT,
+        related_name="customer",
+        verbose_name=_("Lead"),
+    )
+    is_active: BooleanField = models.BooleanField(
+        default=True,
+        verbose_name=_("Is Active"),
+    )
+
+    def __str__(self) -> str:
+        return f"Customer: {self.lead.full_name}"
