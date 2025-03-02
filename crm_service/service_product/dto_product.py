be3a588158224ebd3c47541833ea7f2af3978bcb
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-03-02 13:11:02 +0400

    added Base DTO class and method convertion fields classes in dict

--- crm_service/service_product/dto_product.py
+++ crm_service/service_product/dto_product.py
@@ -1,18 +1,19 @@
 from dataclasses import dataclass
+from typing import Optional
+
+
+# TODO посмотреть как работает библиотека Auto-Dataclass
+#  https://github.com/OleksandrZhydyk/Auto-Dataclass
+
+@dataclass
+class BaseProductDto:
+    def to_dict(self) -> dict[str, Optional[str]]:
+        return {key: value for key, value in self.__dict__.items()}
 
 
 @dataclass
-class ProductDTO:
+class ProductDTO(BaseProductDto):
     pk: int
-    name: str
-    description: str
-    cost: float
-    discount: int
-    status: str
-
-
-@dataclass
-class ProductCreateDTO:
     name: str
     description: str
     cost: float
@@ -20,3 +21,26 @@ class ProductCreateDTO:
     status: str
     archived: bool
     created_by: int
+
+
+@dataclass
+class ProductCreateDTO(BaseProductDto):
+    name: str
+    description: str
+    cost: float
+    discount: int
+    status: str
+    archived: bool
+    created_by: int
+
+
+@dataclass
+class ProductUpdateDTO(BaseProductDto):
+    pk: Optional[int] = None
+    name: Optional[str] = None
+    description: Optional[str] = None
+    cost: Optional[float] = None
+    discount: Optional[int] = None
+    status: Optional[str] = None
+    archived: Optional[bool] = None
+    created_by: Optional[int] = None
