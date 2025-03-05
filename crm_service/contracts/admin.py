1f743a9546d8dee359ff2baf2fab551223111101
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-03-05 23:22:25 +0400

    add model contract in admin panel

--- crm_service/contracts/admin.py
+++ crm_service/contracts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contract
+
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "name",
+        "product",
+        "cost",
+        "start_date",
+        "end_date",
+        "file_document",
+    )
+    search_fields = ("name",)
+    list_display_links = ("name",)
