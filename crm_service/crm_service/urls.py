27c40ec3fd5271aae56bc2ea425444fb1ff9f057
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-03-05 23:26:29 +0400

    added a handler for media and static files and add router for views contracts

--- crm_service/crm_service/urls.py
+++ crm_service/crm_service/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 from .views import HomePageView
+
 
 # TODO добавить i18n_patterns
 urlpatterns = [
@@ -29,6 +32,9 @@ urlpatterns = [
     path("ads/", include("ads.urls")),
     path("leads/", include("leads.urls")),
     path("customers/", include("customers.urls")),
-    # path("contracts/", include("contracts.urls")),
+    path("contracts/", include("contracts.urls")),
     # path("reg/", include("django.contrib.auth.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
