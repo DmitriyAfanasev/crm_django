02533ccfce47c26ff25004d646624862d605a2d9
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-03-06 18:25:17 +0400

    added docstring for DTO Lead

--- crm_service/leads/dto_lead.py
+++ crm_service/leads/dto_lead.py
@@ -1,16 +1,30 @@
-from django.contrib.auth.models import User
-
 from dataclasses import dataclass
-from typing import Optional
 
-from ads.models import AdsCompany
+from typing import Optional, TYPE_CHECKING
 
 from core.base import BaseDTO
+
+if TYPE_CHECKING:
+    from ads.models import AdsCompany
+    from django.contrib.auth.models import User
 
 
 @dataclass
 class BaseLeadDTO(BaseDTO):
-    """Базовый DTO класс Лида."""
+    """
+    Data Transfer Object (DTO) для представления базовой информации о лиде.
+
+    Attributes:
+        pk (Optional[int]): Уникальный идентификатор лида. Может быть None для новых лидов.
+        first_name (str): Имя лида.
+        middle_name (Optional[str]): Отчество лида. Может быть None.
+        last_name (str): Фамилия лида.
+        email (str): Электронная почта лида.
+        phone_number (str): Номер телефона лида.
+        campaign ("AdsCompany"): Рекламная компания, связанная с лидом.
+        created_by (Optional[User]): Пользователь, создавший лид. Может быть None.
+        updated_by (Optional[User]): Пользователь, обновивший лида. Может быть None.
+    """
 
     pk: Optional[int]
     first_name: str
@@ -18,21 +32,36 @@ class BaseLeadDTO(BaseDTO):
     last_name: str
     email: str
     phone_number: str
-    campaign: AdsCompany
-    created_by: User
+    campaign: "AdsCompany"
+    created_by: Optional[User]
     updated_by: Optional[User]
 
 
 @dataclass
 class LeadCreateDTO(BaseLeadDTO):
-    """DTO класс без поля pk, так как лид ещё не создан."""
+    """
+    Data Transfer Object (DTO) для создания лида.
+
+    Наследует все атрибуты BaseLeadDTO, но требует указания created_by.
+
+    Attributes:
+        created_by (User): Пользователь, создавший лид.
+    """
 
     created_by: User
 
 
 @dataclass
 class LeadUpdateDTO(LeadCreateDTO):
-    """Класс для передачи лида с обновлёнными данными, updated_by становится обязательным."""
+    """
+    Data Transfer Object (DTO) для обновления лида.
+
+    Наследует все атрибуты LeadCreateDTO, но требует указания pk и updated_by.
+
+    Attributes:
+        pk (int): Уникальный идентификатор лида.
+        updated_by (User): Пользователь, обновивший лид.
+    """
 
     pk: int
     updated_by: User
