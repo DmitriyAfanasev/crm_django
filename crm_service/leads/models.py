2152dfad360bec7ed8dae130447dd0d508e01b53
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-03-12 18:18:31 +0400

    added docstrings for app leads

--- crm_service/leads/models.py
+++ crm_service/leads/models.py
@@ -1,23 +1,22 @@
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
-
-from django.db import models
-
 from utils.mixins import TimestampMixin, ActorMixin
 from ads.models import AdsCompany
 
 
-# TODO Оператор может создавать, просматривать и редактировать потенциальных клиентов.
 class Lead(TimestampMixin, ActorMixin):
     """
     Модель перспективного клиента, но ещё не активного.
+
     Attributes:
-        first_name (str): Имя
-        middle_name (str): Отчество
-        last_name (str): Фамилия
-        phone_number (PhoneNumber): Номер телефона
-        email (str): Электронная почта
-        campaign (AdsCompany): Рекламная компания из которой он узнал об услуге.
+        first_name (str): Имя.
+        middle_name (str): Отчество.
+        last_name (str): Фамилия.
+        phone_number (PhoneNumber): Номер телефона.
+        email (str): Электронная почта.
+        campaign (AdsCompany): Рекламная компания, из которой он узнал об услуге.
+        is_active (bool): Статус активности лида.
     """
 
     first_name = models.CharField(
@@ -50,6 +49,13 @@ class Lead(TimestampMixin, ActorMixin):
     )
 
     @property
+    def name(self) -> str:
+        """Возвращает адрес электронной почты лида."""
+        # чтобы при удалении лида, в сообщение было понятно, какой объект был удалён
+
+        return self.email
+
+    @property
     def full_name(self) -> str:
         """Возвращает полное имя клиента."""
         if self.middle_name:
@@ -59,16 +65,18 @@ class Lead(TimestampMixin, ActorMixin):
     @property
     def abbreviated_name(self) -> str:
         """
-        Возвращает имя и отчество в виде инициалов с точкой не сокращая фамилию.
-        Examples:
-            Иван Сергеевич Иванов =>\n
-            И. С. Иванов\n
-            Иван Иванов =>\n
-            И. Иванов
+        Возвращает имя и отчество в виде инициалов с точкой, не сокращая фамилию.
+
+        Примеры:
+            Иван Сергеевич Иванов => И. С. Иванов
+            Иван Иванов => И. Иванов
         """
-        first_name = f"{self.first_name[0].capitalize()}."
-        middle_name = f"{self.middle_name[0].capitalize()}." if self.middle_name else ""
-        return f"{first_name} {middle_name} {self.last_name}"
+        first_name_initial = f"{self.first_name[0].capitalize()}."
+        middle_name_initial = (
+            f"{self.middle_name[0].capitalize()}." if self.middle_name else ""
+        )
+        return f"{first_name_initial} {middle_name_initial} {self.last_name}".strip()
 
     def __str__(self) -> str:
+        """Возвращает полное имя лида."""
         return self.full_name
