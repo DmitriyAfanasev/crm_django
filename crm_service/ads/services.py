42de206e3b224f40f658138c398ca3575e15c042
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-03-12 15:59:21 +0400

    added docstrings form services by Company. And also new method for statistics

--- crm_service/ads/services.py
+++ crm_service/ads/services.py
@@ -3,16 +3,22 @@ from django.db.models import Sum
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 
-from pydantic import EmailStr, Field
+from pydantic import EmailStr
 
 import requests
 from requests import RequestException, Response
 
 from ads.dto_ads_company import AdsCompanyCreateDTO
 from ads.models import AdsCompany
+
+from typing import TYPE_CHECKING
+
 from service_product.models import Product
 from utils.mixins.services_mixins import BadWordsMixin
 from core.check_user_service import UserRoleService
+
+if TYPE_CHECKING:
+    from contracts.models import Contract
 
 
 # TODO можно будет добавить рейтинг для заказчиков, и если у них хороший рейтинг, делать скидку или брать их заказы из очереди первыми.
@@ -25,10 +31,11 @@ class AdsCompanyService(BadWordsMixin, UserRoleService):
     email: EmailStr
     created_by: User
     website: str
-    role: str = Field(default="marketer")
 
     @staticmethod
     def checking_before_creation(ads_company_dto: AdsCompanyCreateDTO) -> None:
+        """Проверяет данные перед созданием рекламной компании."""
+
         AdsCompanyService._check_existing_name_by_field_in_db(
             AdsCompany,
             ads_company_dto.name,
@@ -54,6 +61,7 @@ class AdsCompanyService(BadWordsMixin, UserRoleService):
 
     @staticmethod
     def _check_worked_website(website: str) -> bool:
+        """Проверяет доступность вебсайта."""
         try:
             response: Response = requests.get(website, timeout=5)
             if response.status_code != 200:
@@ -64,28 +72,56 @@ class AdsCompanyService(BadWordsMixin, UserRoleService):
 
     @staticmethod
     def get_leads_count(company) -> int:
-        """Количество лидов компании"""
+        """Подсчитывает и возвращает количество лидов компании"""
         return company.leads.count()
 
     @staticmethod
     def get_customers_count(company) -> int:
-        """Количество конвертированных клиентов"""
+        """Подсчитывает и возвращает количество активных клиентов"""
         return company.leads.filter(customer__isnull=False).count()
 
     @staticmethod
-    def calculate_profit(company) -> float:
-        """Расчет процентной прибыли компании."""
-        Contract = apps.get_model("contracts.Contract")
+    def calculate_profit(company: AdsCompany) -> float:
+        """
+        Расчет прибыли компании.
 
-        total_income = (
-            Contract.objects.filter(customer__lead__campaign=company).aggregate(
+        Прибыль = Доход - Затраты.
+        Args:
+            company (AdsCompany): Объект рекламной компании.
+        Returns:
+            float: Прибыль.
+        """
+        contract: type["Contract"] = apps.get_model("contracts", "Contract")
+
+        total_income: int = (
+            contract.objects.filter(customer__lead__campaign=company).aggregate(
                 income=Sum("cost")
             )["income"]
             or 0
-        )  # Защита от None
+        )
+        profit: float = total_income - company.budget
+        return round(profit, 2)
 
+    @staticmethod
+    def calculate_roi(company: AdsCompany) -> float:
+        """Расчет соотношения доходов к затратам.
+
+        ROI = Доход / Затраты.
+
+        Args:
+            company (AdsCompany): Объект рекламной компании.
+        Returns:
+            float: Соотношение доходов к затратам.
+        """
+        contract: type["Contract"] = apps.get_model("contracts.Contract")
+
+        total_income: int = (
+            contract.objects.filter(customer__lead__campaign=company).aggregate(
+                income=Sum("cost")
+            )["income"]
+            or 0
+        )
         if company.budget == 0:
             return 0.0
-
-        profit_percentage = ((total_income - company.budget) / company.budget) * 100
-        return round(profit_percentage, 2)
+        roi: float = total_income / company.budget
+        return round(roi, 2)
