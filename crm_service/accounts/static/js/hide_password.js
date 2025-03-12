21fc6bb227d5918f862455bb8c4ff5e527587a1e
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-03-12 16:02:12 +0400

    added the ability to see the password if necessary

---
+++ crm_service/accounts/static/js/hide_password.js
@@ -0,0 +1,14 @@
+document.getElementById('togglePassword').addEventListener('click', function () {
+    const passwordField = document.getElementById('id_password');
+    const eyeIcon = document.getElementById('eyeIcon');
+
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordField.type = 'password';
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+    }
+});
