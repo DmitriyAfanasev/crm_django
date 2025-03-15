152f608046f862c1f3b30cda00aeae19624ab7cb
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-03-15 19:05:19 +0400

    changed inheritance from ValueError to ValidationError

--- crm_service/exception/exc.py
+++ crm_service/exception/exc.py
@@ -1,3 +1,6 @@
-class ForbiddenWordException(ValueError):
+from django.core.exceptions import ValidationError
+
+
+class ForbiddenWordException(ValidationError):
     def __init__(self, field_name: str):
         super().__init__(f"{field_name.capitalize()} contains forbidden words.")
